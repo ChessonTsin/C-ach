7dfc7ddf105fd35e0770229f0d8f0c853ec33420
Chesson Tsin <m13533293384@163.com>
2018-09-07 18:51:49 +0800

    init gshop

---
+++ src/App.vue
@@ -0,0 +1,21 @@
+<template>
+    <div>
+        <router-view/>
+        <FooterGuide v-show="$route.meta.showFooter"></FooterGuide>
+    </div>
+</template>
+
+<script>
+    import FooterGuide from './components/FooterGuide/FooterGuide.vue'
+
+    export default {
+        components: {
+            FooterGuide
+        }
+    }
+</script>
+
+<style lang="stylus" rel="stylesheet/stylus">
+    // .app
+    //     color red 
+</style>
